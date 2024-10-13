af371fa30320001451b0adafc2329d678cfca468
hakehardware <alex@alexhake.com>
2024-10-13 14:39:15 -0700

    latest

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 // script.js
+let fakePiB = 0
 
 async function fetchSpacePledged() {
     try {
@@ -18,6 +19,24 @@ function bytesToPiB(bytes) {
     return pib.toFixed(3); // Ensure 3 decimal places for precision
 }
 
+function launchConfetti() {
+    confetti({
+      particleCount: 200, // Number of confetti particles
+      spread: 70, // Spread angle
+      origin: { y: 0.6 } // Confetti will launch from this height (near the rocket)
+    });
+  }
+
+// Function to show the banner and start text scrolling
+function showBanner() {
+    const banner = document.querySelector('.space-race-banner');
+    banner.style.display = 'flex'; // Make the banner visible
+  }
+
+  function roundDownToTwoDecimals(value) {
+    return Math.ceil(value * 100) / 100;
+  }
+
 function updateThermometer(pib) {
     const maxPiB = 20;
     const percentage = Math.min((pib / maxPiB) * 100, 100);
@@ -26,11 +45,23 @@ function updateThermometer(pib) {
     const translateY = -(percentage / 100) * window.innerHeight;
 
     rocket.style.transform = `translateY(${translateY}px)`;
+    const spacePledgedElement = document.getElementById('spacePledged');
+    spacePledgedElement.textContent = `Space Pledged: ${roundDownToTwoDecimals(pib)}PiB (${Math.round(percentage)}%)`;
+    const progressBar = document.getElementById('verticalProgressBar');
+
+    progressBar.style.height = `${percentage}%`;
+    spacePledgedElement.style.bottom = `${percentage}%`;
+
+    if (percentage >= 100) {
+        showBanner();
+        launchConfetti();
+    }
 }
 
 
-climbInterval = setInterval(fetchSpacePledged, 1000);
-fetchSpacePledged();
+
+climbInterval = setInterval(fetchSpacePledgedTest, 1000);
+fetchSpacePledgedTest();
 
 // Add stars to the background, restricted to the top 20% of the viewport
 const starsContainer = document.createElement('div');
@@ -38,6 +69,8 @@ starsContainer.classList.add('stars');
 document.querySelector('.rocket-container').appendChild(starsContainer);
 
 const numStars = 100; // Number of stars to generate
+
+let hasLaunched = 0
 
 for (let i = 0; i < numStars; i++) {
   const star = document.createElement('div');
@@ -48,3 +81,9 @@ for (let i = 0; i < numStars; i++) {
 }
 
 
+function fetchSpacePledgedTest() {
+    if (fakePiB < 20) {
+        fakePiB = fakePiB + 0.1;
+    }
+    updateThermometer(fakePiB);
+}
